ee842c3091d34672c1107984ca72f8365c141b3c
DiegoFanBoy <128657618+DiegoFanBoy@users.noreply.github.com>
2023-03-25 23:49:17 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let student = 20;
+let teacher = 3;
+console.log(student + teacher);
+console.log(student / teacher);
+let extraStudents = student % 3;
+console.log(extraStudents);
+document.getElementById("myButton").onclick = function(){
+
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("label1").innerHTML = "Helllo gi" + username
+}
